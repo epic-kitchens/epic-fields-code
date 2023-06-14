0a69a551952143bb4729faaf6510e6b531252e44
Zhifan Zhu <ktw361@gmail.com>
2023-06-14 01:16:54 +0100

    add visualisation codes

--- README.md
+++ README.md
@@ -2,10 +2,51 @@
 
 # Format
 
+- The `camera` parameters use the COLMAP format, which is the same as the OpenCV format.
+- The `images` stores the world-to-camera transformation, represented by quaternion and translation. 
+    - Note: for NeRF usage this needs to be converted to camera-to-world transformation and possibly changing (+x, +y, +z) to (+x, -y, -z)
+- The `points` is part of COLMAP output. It's kept here for visualisation purpose and potentially for computing the `near`/`far` bounds in NeRF input.
+```json
+{
+    'camera': {
+        'id': 1, 'model': 'OPENCV', 'width': 456, 'height': 256,
+        'params': [fx, fy, cx, cy, k1, k2, p1, p2]
+    },
+    'images': {
+        frame_name: [qw, qx, qy, qz, tx, ty, tz],
+        ...
+    },
+    'points': [
+        [x, y, z, r, g, b],
+        ...
+    ]
+}
+
+example data can be found in `example_data/P28_101.json`
+```
+
 # Visualisation
 
 ## Visualise camera poses and pointcloud 
 
 This script requires Open3D. This script is tested with Open3D==0.16.1.
+```python
+python tools/visualise_data_open3d.py --json-data example_data/P28_101.json
+```
+PS: Press 'h' to see the Open3D help message.
 
-## Project a 3D line onto epic-kitchens images using camera poses
+## Example: Project a 3D line onto epic-kitchens images using camera poses
+
+```python
+python tools/project_3d_line.py --json-data example_data/P28_101.json
+```
+
+To visualise the 3D line, use
+```python
+python tools/project_3d_line.py \
+    --json-data example_data/P28_101.json \
+    --line-data example_data/P28_101_line.json \
+    --frames-root example_data/P28_101/
+```
+
+To draw a 3D line, one option is to download the COLMAP format data and use COLMAP GUI to click on points.
---
+++ tools/visualise_data_open3d.py
@@ -0,0 +1,110 @@
+import open3d as o3d
+import numpy as np
+from argparse import ArgumentParser
+import json
+
+from tools.common_functions import get_c2w
+
+""" Visualize poses and point-cloud stored in json file."""
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--json-data', help='path to json data', required=True)
+    parser.add_argument('--line-data', help='path to line data', default=None)
+    parser.add_argument(
+        '--num-display-poses', type=int, default=500, 
+        help='randomly display num-display-poses to avoid creating too many poses')
+    parser.add_argument('--frustum-size', type=float, default=0.1)
+    return parser.parse_args()
+
+
+def get_frustum(c2w: np.ndarray,
+                sz=0.2, 
+                camera_height=None,
+                camera_width=None,
+                frustum_color=[1, 0, 0]) -> o3d.geometry.LineSet:
+    """
+    Args:
+        c2w: np.ndarray, 4x4 camera-to-world matrix
+        sz: float, size (width) of the frustum
+    Returns:
+        frustum: o3d.geometry.TriangleMesh
+    """
+    cen = [0, 0, 0]
+    wid = sz
+    if camera_height is not None and camera_width is not None:
+        hei = wid * camera_height / camera_width
+    else:
+        hei = wid
+    tl = [wid, hei, sz]
+    tr = [-wid, hei, sz]
+    br = [-wid, -hei, sz]
+    bl = [wid, -hei, sz]
+    points = np.float32([cen, tl, tr, br, bl])
+    lines = [
+        [0, 1], [0, 2], [0, 3], [0, 4],
+        [1, 2], [2, 3], [3, 4], [4, 1],]
+    frustum = o3d.geometry.LineSet()
+    frustum.points = o3d.utility.Vector3dVector(points)
+    frustum.lines = o3d.utility.Vector2iVector(lines)
+    frustum.colors = o3d.utility.Vector3dVector([np.asarray([1, 0, 0])])
+    frustum.paint_uniform_color(frustum_color)
+
+    frustum = frustum.transform(c2w)
+    return frustum
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    frustum_size = args.frustum_size
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    with open(args.json_data, 'r') as f:
+        model = json.load(f)
+
+    """ Points """
+    points = model['points']
+    pcd_np = [v[:3] for v in points]
+    pcd_rgb = [np.asarray(v[3:6]) / 255 for v in points]
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pcd_np)
+    pcd.colors = o3d.utility.Vector3dVector(pcd_rgb)
+    vis.add_geometry(pcd, reset_bounding_box=True)
+
+    """ Camear Poses """
+    camera = model['camera']
+    cam_h, cam_w = camera['height'], camera['width']
+    c2w_list = [get_c2w(img) for img in model['images'].values()]
+    c2w_sel_inds = np.random.choice(
+        len(c2w_list), min(len(c2w_list), args.num_display_poses), replace=False)
+    c2w_sel = [c2w_list[i] for i in c2w_sel_inds]
+    frustums = [
+        get_frustum(c2w, sz=frustum_size, camera_height=cam_h, camera_width=cam_w) 
+        for c2w in c2w_sel
+    ]
+    for frustum in frustums:
+        vis.add_geometry(frustum, reset_bounding_box=True)
+    
+    """ Optional: Line """
+    if args.line_data is not None:
+        line_set = o3d.geometry.LineSet()
+        with open(args.line_data, 'r') as f:
+            line_points = np.asarray(json.load(f)).reshape(2, 3)
+        vc = line_points.mean(axis=0)
+        dir = line_points[1] - line_points[0]
+        lst = vc + 2 * dir
+        led = vc - 2 * dir
+        lines = [lst, led]
+        line_set.points = o3d.utility.Vector3dVector(lines)
+        line_set.lines = o3d.utility.Vector2iVector([[0, 1]])
+        vis.add_geometry(line_set, reset_bounding_box=True)
+
+    control = vis.get_view_control()
+    control.set_front([1, 1, 1])
+    control.set_lookat([0, 0, 0])
+    control.set_up([0, 0, 1])
+    control.set_zoom(1)
+
+    vis.run()
+    vis.destroy_window()
